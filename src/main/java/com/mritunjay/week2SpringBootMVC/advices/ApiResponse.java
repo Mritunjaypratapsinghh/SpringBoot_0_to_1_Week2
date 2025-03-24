cf30b5146ffbce46234d62e2eba65a9787533029
Mritunjay Pratap Singh <mritunjaypratapsinghh@gmail.com>
2025-03-24 18:33:46 +0530

    Implement ApiResponse class for consistent API response handling
    - Added constructors in ApiResponse to handle success and error scenarios.
      - When a response contains valid data, the data field is populated, and error is set to null.
      - When an error occurs, the error field is populated, and data is set to null.
    - Included timestamp formatting using @JsonFormat to standardize the response time format.
    - Improved API response structure to enhance client parsing and simplify error handling.

--- src/main/java/com/mritunjay/week2SpringBootMVC/advices/ApiResponse.java
+++ src/main/java/com/mritunjay/week2SpringBootMVC/advices/ApiResponse.java
@@ -1,4 +1,31 @@
 package com.mritunjay.week2SpringBootMVC.advices;
 
-public class ApiResponse {
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ApiResponse<T> {
+//    @Pattern(regexp = "hh-mm-ss dd-mm-yy")
+    @JsonFormat(pattern = "hh:mm:ss a dd-MM-yyyy")
+    private LocalDateTime timestamp;
+    private T data;
+    private ApiError error;
+
+    public ApiResponse(){
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public ApiResponse(T data){
+        this();
+        this.data = data;
+    }
+
+    public ApiResponse(ApiError error){
+        this();
+        this.error = error;
+    }
+
 }
