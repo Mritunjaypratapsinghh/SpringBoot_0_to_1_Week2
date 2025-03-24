896e60fd66841fe6ad11418f91f23a54e4626132
Mritunjay Pratap Singh <mritunjaypratapsinghh@gmail.com>
2025-03-24 17:28:02 +0530

    feat(validation): add custom annotation and validator for Employee Role validation
    - Introduced @EmployeeRoleValidation annotation to enforce allowed roles (USER, ADMIN)
    - Implemented EmployeeRoleValidator class to handle role validation logic
    - Enhances data validation and ensures input constraints for Employee entity

--- src/main/java/com/mritunjay/week2SpringBootMVC/annotations/EmployeeRoleValidator.java
+++ src/main/java/com/mritunjay/week2SpringBootMVC/annotations/EmployeeRoleValidator.java
@@ -1,4 +1,14 @@
 package com.mritunjay.week2SpringBootMVC.annotations;
 
-public class EmployeeRoleValidator {
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.util.List;
+
+public class EmployeeRoleValidator implements ConstraintValidator<EmployeeRoleValidation,String> {
+    @Override
+    public boolean isValid(String inputRole, ConstraintValidatorContext constraintValidatorContext) {
+        List<String>roles = List.of("USER","ADMIN");
+        return roles.contains(inputRole);
+    }
 }
